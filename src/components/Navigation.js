9cf13af6ca282a706de1bbe576230cae2381537e
sourabbanka22 <sourabbanka22@gmail.com>
2020-10-06 23:58:59 +0530

    Styling the Navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,19 +3,22 @@ import { NavLink } from 'react-router-dom';
 
 function Navigation() {
     return (
+        
         <nav id="navigation">
-            <ul>
-                <li><NavLink id="home-button" to="/" exact>Home</NavLink></li>
-                <li><NavLink to="/s/programming" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Programming</NavLink></li>
-                <li><NavLink to="/s/technologies" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Technologies</NavLink></li>
-                <li><NavLink to="/s/education" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Education</NavLink></li>
-                <li><NavLink to="/s/job" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Job</NavLink></li>
-                <li><NavLink to="/s/personal" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Personal</NavLink></li>
-                <li><NavLink to="/s/certificates" exact activeStyle={{ 'backgroundColor':'hsl(219, 86%, 57%)','color':'hsl(0, 0%, 100%)' }}>Certificates</NavLink></li>
-                <li><hr id="separator"/></li>
-                <li><NavLink to="/s/result" exact activeStyle={{ 'backgroundColor':'hsl(125, 75%, 41%)','color':'hsl(0, 0%, 100%)' }}>Result</NavLink></li>
-            </ul>
+            <div className="side-nav">
+                <NavLink id="home-button" to="/" exact> Home </NavLink>
+                <ul className="style-remover list-hover-slide">
+                    <li> <NavLink className="link" to="/s/programming" exact> Programming </NavLink></li>
+                    <li> <NavLink className="link" to="/s/technologies" exact> Technologies </NavLink></li>
+                    <li> <NavLink className="link" to="/s/education" exact> Education </NavLink></li>
+                    <li> <NavLink className="link" to="/s/job" exact> Job </NavLink></li>
+                    <li> <NavLink className="link" to="/s/personal" exact> Personal </NavLink></li>
+                    <li> <NavLink className="link" to="/s/certificates" exact> Certificates </NavLink></li>
+                </ul>
+                <NavLink id="result-button" to="/s/result" exact> Result </NavLink>
+            </div>
         </nav>
+
     );
 }
 
