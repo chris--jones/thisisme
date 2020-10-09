01ac124dce34f6033b8362c4a613fa866053957e
sourabbanka22 <sourabbanka22@gmail.com>
2020-10-09 12:52:42 +0530

    Making the Sidebar Collapsible

---
+++ src/components/icons/index.js
@@ -0,0 +1,5 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faUniversity, faCode, faUserMd, faCertificate, faUser, faBackward, faBars, faTimes, faListAlt } from '@fortawesome/free-solid-svg-icons'
+ 
+library.add(fab, faUniversity, faCode, faUserMd, faCertificate, faUser, faBackward, faBars, faTimes, faListAlt)
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,25 +1,62 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "./Navigation.scss";
 
 function Navigation() {
     return (
         
-        <nav id="navigation">
-            <div className="side-nav">
-                <NavLink id="home-button" to="/" exact> Home </NavLink>
-                <ul className="style-remover list-hover-slide">
-                    <li> <NavLink className="link" to="/s/programming" exact> Programming </NavLink></li>
-                    <li> <NavLink className="link" to="/s/technologies" exact> Technologies </NavLink></li>
-                    <li> <NavLink className="link" to="/s/education" exact> Education </NavLink></li>
-                    <li> <NavLink className="link" to="/s/job" exact> Job </NavLink></li>
-                    <li> <NavLink className="link" to="/s/personal" exact> Personal </NavLink></li>
-                    <li> <NavLink className="link" to="/s/certificates" exact> Certificates </NavLink></li>
-                </ul>
-                <NavLink id="result-button" to="/s/result" exact> Result </NavLink>
+        <div>
+            <NavLink className="home-button" to="/" exact>
+                <FontAwesomeIcon icon="backward" className="icon" /> 
+            </NavLink>
+            <div className="wrapper">
+                <input type="checkbox" id="btn" hidden/>
+                <label for="btn" className="menu-btn">
+                    <FontAwesomeIcon icon="bars" className="icon" />
+                    <FontAwesomeIcon icon="times" className="icon" /> </label>
+                <div id="sidebar">
+                    <div className="title">thisisme</div>
+                    <ul className="list-items">
+                        <li>
+                            <NavLink className="link" to="/s/programming" exact>
+                                <FontAwesomeIcon icon="code" className="icon" /> Programming 
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink className="link" to="/s/technologies" exact>
+                                <FontAwesomeIcon icon={[ 'fab', 'node-js']} className="icon" /> Technologies 
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink className="link" to="/s/education" exact>
+                                <FontAwesomeIcon icon="university" className="icon" /> Education 
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink className="link" to="/s/job" exact>
+                                <FontAwesomeIcon icon="user-md" className="icon" /> Job 
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink className="link" to="/s/personal" exact>
+                                <FontAwesomeIcon icon="user" className="icon" /> Personal 
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink className="link" to="/s/certificates" exact>
+                                <FontAwesomeIcon icon="certificate" className="icon" /> Certificates 
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink className="link" to="/s/result" exact>
+                                <FontAwesomeIcon icon="list-alt" className="icon" /> Result 
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
             </div>
-        </nav>
+        </div>
 
     );
 }
