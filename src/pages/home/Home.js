3aa76fb29b6bf7763640d5ea01a31138d6c75b22
Jose De Freitas <joselocotote26@gmail.com>
2020-10-12 15:55:27 -0500

    💄 Add images to home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,9 +4,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Footer from '../../components/footer/Footer';
 import "./Home.scss";
 
+import ImageProgramming from '../../images/home-image-programming.svg'
+import ImageTechnologies from '../../images/home-image-technologies.svg'
+import ImageEducation from '../../images/home-image-education.svg'
+import ImageJob from '../../images/home-image-job.svg'
+import ImagePersonal from '../../images/home-image-personal.svg'
+import ImageCertificates from '../../images/home-image-certificates.svg'
+
 function Home() {
     return (
         <div id="home">
+            <div id="home-images">
+                <img className="home-image" src={ImageProgramming} alt="Image Programming section preview"/>
+                <img className="home-image" src={ImageTechnologies} alt="Image Technologies section preview"/>
+                <img className="home-image" src={ImageEducation} alt="Image Education section preview"/>
+                <img className="home-image" src={ImageJob} alt="Image Job section preview"/>
+                <img className="home-image" src={ImagePersonal} alt="Image Personal section preview"/>
+                <img className="home-image" src={ImageCertificates} alt="Image Certificates section preview"/>
+            </div>
             <a id="version-text" href="https://github.com/JoseDeFreitas/thisisme/releases" target="_blank" rel="noopener noreferrer">v1.0.0</a>
             <h1>thisisme</h1>
             <p>Create cool images containing useful<br/>information about you and your work<br/>for your GitHub readme</p>
